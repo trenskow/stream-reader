48cc5caf738d74121fe2c4fd4cc77d3fdbf2d885
Kristian Trenskow <this.is@kristians.email>
2025-01-02 21:50:44 +0100

    Removed the ability to limit streams – use with @trenskow/stream-limiter instead.

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-export default (stream, options = {}) => {
+export default (stream) => {
 
 	return new Promise((resolve, reject) => {
-
-		let rejected = false;
 
 		let result = new Buffer.alloc(0);
 
 		stream.on('data', (buffer) => {
-			if (rejected) return;
 			result = Buffer.concat([result, buffer]);
-			if (typeof options.limit === 'number' && result.length > options.limit) {
-				rejected = true;
-				reject(options.error ? options.error() : new Error('Stream length limit reached.'));
-			}
 		});
 
-		stream.on('end', () => resolve(result));
-		stream.on('error', (err) => {
-			if (!rejected) reject(err);
+		stream.once('end', () => resolve(result));
+
+		stream.once('error', (err) => {
+			reject(err);
 		});
 
 	});
